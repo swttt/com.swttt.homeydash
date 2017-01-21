3f312a56f426cc3d6e7bbfe56194848b93762b22
swttt <bas@MacBook-van-Bas.fritz.box>
2017-01-21 22:04:57 +0100

    token test

--- api.js
+++ api.js
@@ -1,28 +1,28 @@
 module.exports = [
 
-    {
-        description: 'Save new settings',
-        method: 'POST',
-        path: '/savesettings',
-        fn: function(callback, args) {
-            var result = Homey.app.saveNewSettings(args.body);
+  {
+    description: 'Save new settings',
+    method: 'POST',
+    path: '/savesettings',
+    fn: function(callback, args) {
+      var result = Homey.app.saveNewSettings(args.body);
 
-            // callback follows ( err, result )
-            callback(null, 'Saved settings');
+      // callback follows ( err, result )
+      callback(null, 'Saved settings');
 
-            // access /?foo=bar as args.query.foo
-        }
-    }, {
-        description: 'Get config.json',
-        method: 'GET',
-        path: '/config.json',
-        fn: function(callback, args) {
-            var result = Homey.app.getConfig();
-
-            // callback follows ( err, result )
-            callback(null, result);
-
-            // access /?foo=bar as args.query.foo
-        }
+      // access /?foo=bar as args.query.foo
     }
+  }, {
+    description: 'Get config.json',
+    method: 'GET',
+    path: '/config.json',
+    fn: function(callback, args) {
+      var result = Homey.app.getConfig();
+
+      // callback follows ( err, result )
+      callback(null, result);
+
+      // access /?foo=bar as args.query.foo
+    }
+  }
 ]
