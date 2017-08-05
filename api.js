7e0d09e77c7218bfa3921f9ee35c81db2042fe6e
Bas Jansen <b@sjansen.email>
2017-08-06 01:42:53 +0200

    Finished auth

--- api.js
+++ api.js
@@ -7,12 +7,18 @@ module.exports = [
     method: 'GET',
     path: '/auth',
     fn: function(args, callback) {
-
-      
-      Homey.app.getDevices().then(res => {
-          callback(null, res);
-        })
-        .catch(error => callback(error, null));
+      // Check if supplied token matches the stored autkey
+      // If they match, get the homeyObject from app.js
+      if(args.query.token === Homey.ManagerSettings.get('authkey')){
+        console.log('Valid auth!');
+        Homey.app.getToken()
+          .then(res => callback(null, res))
+          .catch(err => callback(err, null))
+      }
+      else{
+        console.log('Invalid auth!')
+        callback('Invalid auth token', null)
+      }
 
     }
   }
--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const Homey = require('homey');
 
+const { HomeyAPI } = require('./lib/athom-api.js')
+
 const randomize = require('randomatic');
 
 class HomeydashApp extends Homey.App {
@@ -9,14 +11,24 @@ class HomeydashApp extends Homey.App {
 	onInit() {
 
 		this.log('Homeydash is running...');
+
+    // Check if random auth key excists in settings
+    // If not, generate a new one
     if(!Homey.ManagerSettings.get('authkey')){
       this.error('No authkey found!')
       Homey.ManagerSettings.set('authkey', randomize('A0', 6))
     }
 	}
 
-  getToken(){
-    
+  // Get the homeyObject needed to auth with athom-api
+  async getToken(){
+    try{
+      const homeyObject = await HomeyAPI.forCurrentHomey();
+      return homeyObject;
+    }
+    catch(err){
+      this.error(err);
+    }
   }
 
 }
